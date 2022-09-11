2839820f1eedfd3dff9e48010c37b9066abf85db
inrhor <inrhor@aliyun.com>
2022-09-11 16:49:11 +0800

    publish🚀:  0.4.0

--- src/main/kotlin/cn/inrhor/imipetcore/api/entity/ai/nms/NmsAiGoal.kt
+++ src/main/kotlin/cn/inrhor/imipetcore/api/entity/ai/nms/NmsAiGoal.kt
@@ -1,27 +1,2 @@
 package cn.inrhor.imipetcore.api.entity.ai.nms
 
-import org.bukkit.entity.Entity
-import taboolib.module.nms.MinecraftVersion
-
-object NmsAiGoal {
-
-    fun Entity.addNmsAi(nmsAi: NmsAi, priority: Int) {
-        try {
-            val nmsEntity = this::class.java.getMethod("getHandle").invoke(this)
-            val v = if (MinecraftVersion.major == 4) "v1_12_R1" else "v1_16_R3"
-            val version = "net.minecraft.server.$v"
-            val pathSelector = Class.forName("$version.PathfinderGoalSelector")
-            val pathGoal = Class.forName("$version.PathfinderGoal")
-            val entityInsentient = Class.forName("$version.EntityInsentient")
-            val method = pathSelector.getDeclaredMethod("a", Int::class.javaPrimitiveType,
-                    pathGoal)
-            val field = entityInsentient.getDeclaredField("goalSelector")
-            field.isAccessible = true
-            val obj = field.get(nmsEntity)
-            method.invoke(obj, priority, nmsAi)
-        }catch (ex: Exception) {
-            ex.printStackTrace()
-        }
-    }
-
-}
