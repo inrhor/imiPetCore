31b57e9957822620b88f05906b54a8a3053a7828
inrhor <inrhor@aliyun.com>
2022-09-11 16:04:50 +0800

    perf👌:  反射NMS

--- src/main/kotlin/cn/inrhor/imipetcore/api/entity/ai/nms/NmsAiGoal.kt
+++ src/main/kotlin/cn/inrhor/imipetcore/api/entity/ai/nms/NmsAiGoal.kt
@@ -1,22 +1,23 @@
 package cn.inrhor.imipetcore.api.entity.ai.nms
 
-import net.minecraft.server.v1_12_R1.EntityInsentient
-import net.minecraft.server.v1_12_R1.PathfinderGoal
-import net.minecraft.server.v1_12_R1.PathfinderGoalSelector
-import org.bukkit.craftbukkit.v1_12_R1.entity.CraftEntity
 import org.bukkit.entity.Entity
+import taboolib.module.nms.MinecraftVersion
 
 object NmsAiGoal {
 
     fun Entity.addNmsAi(nmsAi: NmsAi, priority: Int) {
-        val e = (this as CraftEntity).handle
         try {
-            val method = PathfinderGoalSelector::class.java
-                .getDeclaredMethod("a", Int::class.javaPrimitiveType,
-                    PathfinderGoal::class.javaObjectType)
-            val field = EntityInsentient::class.java.getDeclaredField("goalSelector")
+            val nmsEntity = this::class.java.getMethod("getHandle").invoke(this)
+            val v = if (MinecraftVersion.major == 4) "v1_12_R1" else "v1_16_R3"
+            val version = "net.minecraft.server.$v"
+            val pathSelector = Class.forName("$version.PathfinderGoalSelector")
+            val pathGoal = Class.forName("$version.PathfinderGoal")
+            val entityInsentient = Class.forName("$version.EntityInsentient")
+            val method = pathSelector.getDeclaredMethod("a", Int::class.javaPrimitiveType,
+                    pathGoal)
+            val field = entityInsentient.getDeclaredField("goalSelector")
             field.isAccessible = true
-            val obj = field.get(e)
+            val obj = field.get(nmsEntity)
             method.invoke(obj, priority, nmsAi)
         }catch (ex: Exception) {
             ex.printStackTrace()
