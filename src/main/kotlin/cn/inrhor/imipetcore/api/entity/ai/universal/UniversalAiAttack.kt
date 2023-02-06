a63daa236f77662be650d3e1f96831e5ceba8697
inrhor <inrhor@aliyun.com>
2023-02-07 02:43:26 +0800

    perf👌:  基于实体伪装，优化了攻击行为

--- src/main/kotlin/cn/inrhor/imipetcore/api/entity/ai/universal/UniversalAiAttack.kt
+++ src/main/kotlin/cn/inrhor/imipetcore/api/entity/ai/universal/UniversalAiAttack.kt
@@ -3,11 +3,7 @@ package cn.inrhor.imipetcore.api.entity.ai.universal
 import cn.inrhor.imipetcore.api.entity.PetEntity
 import cn.inrhor.imipetcore.api.manager.MetaManager.metaEntity
 import cn.inrhor.imipetcore.api.manager.MetaManager.removeMeta
-import cn.inrhor.imipetcore.api.manager.ModelManager.playAnimation
-import cn.inrhor.imipetcore.api.manager.OptionManager.model
 import cn.inrhor.imipetcore.common.location.distanceLoc
-import org.bukkit.entity.LivingEntity
-import taboolib.module.ai.navigationMove
 
 class UniversalAiAttack(val petEntity: PetEntity, val action: String = "attack", var delay: Int = 0): UniversalAi() {
 
@@ -18,34 +14,17 @@ class UniversalAiAttack(val petEntity: PetEntity, val action: String = "attack",
     }
 
     override fun startTask() {
-        delay = petEntity.petData.attribute.attack_speed*20
     }
 
     override fun continueExecute(): Boolean {
         val target = petEntity.entity?.metaEntity("target")?: return false
-        return (petEntity.entity?.distanceLoc(petEntity.owner)?: 20.0) < 10.0 && !target.isDead
+        return (petEntity.entity?.distanceLoc(petEntity.owner)?: 20.0) < 15.0 && !target.isDead
     }
 
     override fun updateTask() {
         val entity = petEntity.entity?: return
-        val petData = petEntity.petData
-        val attribute = petData.attribute
         val target = entity.metaEntity("target")?: return
-        entity.navigationMove(target.location, attribute.speed)
-        if (entity.distanceLoc(target)<= 3.0) {
-            if (petData.attribute.attack_speed*20 == delay) {
-                (target as LivingEntity).damage(attribute.attack, entity)
-                val attackOption = petEntity.getStateOption(action)
-                if (attackOption != null) {
-                    val model = petEntity.model()
-                    entity.playAnimation(model.id, model.select, action, attackOption)
-                }
-            }else if (delay == 0) {
-                delay = petEntity.petData.attribute.attack_speed*20
-                return
-            }
-            delay--
-        }
+        entity.attack(target)
     }
 
     override fun resetTask() {
